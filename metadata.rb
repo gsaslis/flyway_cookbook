1aa43b55a8931061e78a92356cc7d278d396cdf1
Aaron Walker <aaronpwalker@gmail.com>
2015-04-01 14:50:58 +1100

    fixed metadata

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,9 @@ name             'flyway'
 maintainer       'base2Services'
 maintainer_email 'a.walker@base2services.com'
 license          'Apache License, Version 2.0'
-description      'Allow for use of flywaydb migrations '
+description      'Installs and configures flywaydb a database migration tool'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
+version          '0.1.2'
 
 supports 'amazon'
 supports 'centos'
