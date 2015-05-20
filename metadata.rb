1585983215f2feeadcc2463312e50cadd6dd5176
Giorgos Saslis <g.saslis@gmail.com>
2015-05-20 18:51:27 +0300

    fix rubocop errors
    attempt to fix travis build

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             'flyway'
-maintainer       'base2Services'
+name 'flyway'
+maintainer 'base2Services'
 maintainer_email 'a.walker@base2services.com'
-license          'Apache License, Version 2.0'
-description      'Installs and configures flywaydb a database migration tool'
+license 'Apache License, Version 2.0'
+description 'Installs and configures flywaydb a database migration tool'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version '0.1.2'
 
 supports 'amazon'
 supports 'centos'
