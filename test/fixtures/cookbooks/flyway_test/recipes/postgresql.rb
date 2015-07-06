4e324f942a7bbe3290a91a61f0ddc6dd32d01d95
Yorgos Saslis <g.saslis@gmail.com>
2015-07-06 17:52:02 +0300

    fix rubocop errors

--- test/fixtures/cookbooks/flyway_test/recipes/postgresql.rb
+++ test/fixtures/cookbooks/flyway_test/recipes/postgresql.rb
@@ -2,7 +2,6 @@ include_recipe 'postgresql::ruby'
 include_recipe 'database::postgresql'
 include_recipe 'postgresql::server'
 
-
 postgresql_connection_info = {
   host: '127.0.0.1',
   port: '5432',
